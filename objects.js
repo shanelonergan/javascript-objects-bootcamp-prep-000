aec8b7087ff8910014464f6304ca17fd80a23382
Shane Lonergan <sptlonergan@gmail.com>
2019-07-02 17:49:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,10 @@ var playlist = {
   'Jimi Hendrix': 'Little Wing',
   'Stevie Ray Vaugn': 'Pride and Joy',
 };
+
+function updatePlaylist(playlist, artist, song) {
+  playlist.artist = song;
+  return playlist;
+}
+
+updatePlaylist(playlist, 'Steve Vai', 'For the Love of God')
